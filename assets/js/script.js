cacda96018aa6ce494094c5a130325a7606093dc
Abby Roberg <abbyroberg@gmail.com>
2021-07-28 14:04:04 -0500

    function to get current weather

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var currentTempEl = document.querySelector("#current-temp");
+var currentWindEl = document.querySelector("#current-wind");
+
+var currentTemp = "";
+var currentWind = "";
+var currentHumid = "";
+var currentUV = "";
+
+// function to get lat/long of given city
+
+// function to get current weather from Open Weather Api for a given lat/long
+
+var getWeather = function(lat, long) {
+    var apiCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=minutely&appid=e76cfd847a87e984b00da68d202f4233";
+    
+    fetch(apiCall).then(function(response) {
+        response.json().then(function(data){
+            currentTemp = data.current.temp;
+            currentWind = data.current.wind_speed;
+            currentHumid = data.current.humidity;
+            currentUV = data.current.uvi;
+            displayCurrentWeather();
+        });
+    })
+}
+
+getWeather(30.26, -97.74);
+
+// function to display current day weather
+
+var displayCurrentWeather = function() {
+    console.log(currentTemp);
+}
+
+// function to display 5-day forecast
